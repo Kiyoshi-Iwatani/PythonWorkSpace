e29350be468610d764e25463c234344dfa97f879
Kiyoshi-Iwatani <takamayukiyo2017@gmail.com>
2021-10-20 12:23:54 +0900

    問題 list4.py

--- 1020/list4.py
+++ 1020/list4.py
@@ -1,6 +1,12 @@
 n=input("整数を入力してください")
 number=range(1,int(n)+1)
-total=sum(number)
-avg=total/len(number)
+total=0
+average=0
+num=int(n)
+i=0
+while i<=num:
+    total+=i
+    i+=1
+average=total/num
 print(f"1~{n}までの合計：{total}")
-print(f"平均：{avg}")
+print(f"平均：{average}")
